9cce65890c81af6b50bf21b491d9bfd2a53d4b2c
kim-su-hwan <64351122+kim-su-hwan@users.noreply.github.com>
2022-05-25 14:01:15 +0900

    asd
    asd

---
+++ Assets/Scripts/CafeteriaScripts/UpdateMoneyTmp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class UpdateMoneyTmp : MonoBehaviour
+{
+    [SerializeField] private TMP_Text money;
+    [SerializeField] private GameObject menu_canvas;
+    [SerializeField] private GameObject addMenu_canvas;
+
+    private void Start()
+    {
+        UpdateMoneyText();
+        OnMenuScene();
+        OffAddMenuScene();
+    }
+    public void UpdateMoneyText()
+    {
+        money.text = $"남은 돈 : {GameManager.instance.GetMoney()}";
+    }
+    public void OffMenuScene()
+    {
+        menu_canvas.SetActive(false);
+    }
+
+    public void OnMenuScene()
+    {
+        menu_canvas.SetActive(true);
+    }
+    public void OffAddMenuScene()
+    {
+        addMenu_canvas.SetActive(false);
+    }
+
+    public void OnAddMenuScene()
+    {
+        addMenu_canvas.SetActive(true);
+    }
+}

13e073a24a8b5d10650001969e95e50296ca31f2
kim-su-hwan <64351122+kim-su-hwan@users.noreply.github.com>
2022-05-29 20:57:46 +0900

    adfaf
    adfafd

--- Assets/Scripts/CanvasControllUI.cs
+++ Assets/Scripts/CanvasControllUI.cs
@@ -47,6 +47,6 @@ public class CanvasControllUI : MonoBehaviour
 
     public void Exit()
     {
-        SceneManager.LoadScene("SchoolScene");
+        SceneManager.LoadScene("GamePlayScene");
     }
 }
